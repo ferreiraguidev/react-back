90fcae12bac8ec08dc8286e52a887b0f4192305c
Guilherme Ferreira <ferreiragui.dev@gmail.com>
2023-06-21 09:53:06 -0300

    entity

---
+++ src/main/java/com/example/reactback/model/Model.java
@@ -0,0 +1,33 @@
+package com.example.reactback.model;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import static jakarta.persistence.GenerationType.IDENTITY;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Model {
+
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String email;
+
+    private String password;
+
+    private String description;
+
+}
