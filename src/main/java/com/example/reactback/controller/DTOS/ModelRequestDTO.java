c5bec52d9f5c908bbfe29007b6ea9f1cca5ce19b
Guilherme Ferreira <ferreiragui.dev@gmail.com>
2023-06-21 09:54:30 -0300

    dto's

---
+++ src/main/java/com/example/reactback/controller/DTOS/ModelRequestDTO.java
@@ -0,0 +1,8 @@
+package com.example.reactback.controller.DTOS;
+
+public record ModelRequestDTO (
+        String name,
+       String email,
+       String  password,
+       String description){
+}
---
+++ src/main/java/com/example/reactback/controller/DTOS/ModelResponseDTO.java
@@ -0,0 +1,8 @@
+package com.example.reactback.controller.DTOS;
+
+public record ModelResponseDTO(
+        String name,
+       String email,
+       String  password,
+       String description){
+}
