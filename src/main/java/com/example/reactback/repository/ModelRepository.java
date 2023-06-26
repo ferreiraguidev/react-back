c7504bcbc1c4a3a7e8d6ffe6ecd5f82107176b87
Guilherme Ferreira <ferreiragui.dev@gmail.com>
2023-06-26 06:01:27 -0300

    updating - new method

--- src/main/java/com/example/reactback/repository/ModelRepository.java
+++ src/main/java/com/example/reactback/repository/ModelRepository.java
@@ -2,7 +2,10 @@ package com.example.reactback.repository;
 
 import com.example.reactback.model.Model;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ModelRepository extends JpaRepository<Model, Long> {
 
+    Model findByLoginIgnoreCase(String login);
 }
