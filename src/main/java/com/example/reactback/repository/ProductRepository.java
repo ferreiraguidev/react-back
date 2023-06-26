057defb818dba0d4661414cab5fcedd1437d9ab4
Guilherme Ferreira <ferreiragui.dev@gmail.com>
2023-06-26 09:27:25 -0300

    Product image Repository

---
+++ src/main/java/com/example/reactback/repository/ProductRepository.java
@@ -0,0 +1,10 @@
+package com.example.reactback.repository;
+
+
+import com.example.reactback.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
