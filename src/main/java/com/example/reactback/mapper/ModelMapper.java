06231fdc5b1f95d1771751776676a64e633f30ac
Guilherme Ferreira <ferreiragui.dev@gmail.com>
2023-06-26 06:07:00 -0300

    ModelMapper update

--- src/main/java/com/example/reactback/mapper/ModelMapper.java
+++ src/main/java/com/example/reactback/mapper/ModelMapper.java
@@ -4,19 +4,22 @@ package com.example.reactback.mapper;
 import com.example.reactback.controller.DTOS.ModelRequestDTO;
 import com.example.reactback.controller.DTOS.ModelResponseDTO;
 import com.example.reactback.model.Model;
+import org.mapstruct.Mapper;
+import org.mapstruct.ReportingPolicy;
 import org.springframework.stereotype.Component;
 
 @Component
+@Mapper(componentModel = "spring",unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public class ModelMapper {
 
 
     public static Model toDomain(final ModelRequestDTO requestDTO){
-        return new Model(null, requestDTO.name(), requestDTO.email(),
-                requestDTO.description(),requestDTO.description());
+        return new Model(null, requestDTO.name(), requestDTO.email(), requestDTO.login(),
+                requestDTO.password(),requestDTO.passwordConfirmation());
     }
 
     public static ModelResponseDTO toDto(final Model model){
-        return new ModelResponseDTO(model.getName(),
-                model.getEmail(),model.getPassword(),model.getDescription());
+        return new ModelResponseDTO(model.getName(), model.getEmail(), model.getLogin(),
+                model.getPassword(),model.getPasswordConfirmation());
     }
 }
